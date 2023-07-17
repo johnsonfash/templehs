867154c333dd9c2a41a9e67b31e13d4a52a0bc67
Johnson Fash <fashanutosin7@gmail.com>
2023-07-17 09:24:17 +0100

    built ui for navbar in layout

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,15 +4,15 @@ import { NextRequest, NextResponse } from "next/server";
 const authRedirects = ['/login', '/register'];
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get('__On_C_deAt')?.value ?? '';
-  const pathname = request.nextUrl?.pathname
-  const { status } = validateToken(token)
-  const url = request.nextUrl.clone()
-  if (authRedirects.includes(pathname) && status) {
-    url.pathname = '/';
-    return NextResponse.redirect(url)
-  } else if (pathname.match(/^\/$|^\/[\d]+/) && !status) {
-    url.pathname = '/login';
-    return NextResponse.redirect(url)
-  }
+  // const token = request.cookies.get('__On_C_deAt')?.value ?? '';
+  // const pathname = request.nextUrl?.pathname
+  // const { status } = validateToken(token)
+  // const url = request.nextUrl.clone()
+  // if (authRedirects.includes(pathname) && status) {
+  //   url.pathname = '/';
+  //   return NextResponse.redirect(url)
+  // } else if (pathname.match(/^\/$|^\/[\d]+/) && !status) {
+  //   url.pathname = '/login';
+  //   return NextResponse.redirect(url)
+  // }
 }
